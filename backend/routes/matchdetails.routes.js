3213a21fb7ce0da5a18c5eef8fc4735cc5c2b962
Stanko Horvat <horvat.stanko@gmail.com>
2025-06-11 22:38:41 +0200

    really dont know what I am doing

--- backend/routes/matchdetails.routes.js
+++ backend/routes/matchdetails.routes.js
@@ -3,5 +3,5 @@ const router = Router();
 import { getMatchDetailsById } from '../controllers/matchdetails.controller.js';
 
 router.get('/:matchTPId', getMatchDetailsById);
-
+console.log('Files here:', fs.readdirSync(path.join(__dirname, '../data/matchDetails')).slice(0,10));
 export default router;
