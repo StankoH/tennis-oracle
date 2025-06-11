dd15d0f7dd6a75057f38e334e7a2a48fc666eb16
Stanko Horvat <horvat.stanko@gmail.com>
2025-06-11 23:11:45 +0200

    ovo je u qrcu

--- backend/routes/matchdetails.routes.js
+++ backend/routes/matchdetails.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 const router = Router();
 import { getMatchDetailsById } from '../controllers/matchdetails.controller.js';
 import fs from 'fs';
+import path from "path";
 
 router.get('/:matchTPId', getMatchDetailsById);
 console.log('Files here:', fs.readdirSync(path.join(__dirname, '../data/matchDetails')).slice(0,10));
