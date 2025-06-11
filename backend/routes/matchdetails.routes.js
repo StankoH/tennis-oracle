361288b6d62b009f4395ba0730f994f1a63c6712
Stanko Horvat <horvat.stanko@gmail.com>
2025-06-11 23:03:43 +0200

    daj me nemoj vise davit sa pisanjem komentara

--- backend/routes/matchdetails.routes.js
+++ backend/routes/matchdetails.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 import { getMatchDetailsById } from '../controllers/matchdetails.controller.js';
+import fs from 'fs';
 
 router.get('/:matchTPId', getMatchDetailsById);
 console.log('Files here:', fs.readdirSync(path.join(__dirname, '../data/matchDetails')).slice(0,10));
